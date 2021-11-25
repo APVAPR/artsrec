0a5502df5c91498c1e1e3ae67b542b8c9c95fd3b
APVAPR <apvapr@github.com>
2021-11-25 14:24:50 +0300

    Add staticfiles

--- artsrec/settings.py
+++ artsrec/settings.py
@@ -16,7 +16,8 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import django_heroku
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -119,10 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
 
